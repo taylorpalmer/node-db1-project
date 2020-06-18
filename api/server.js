6881913c85d8b5bbe485f9473f040ff164ffc0fc
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-17 18:38:17 -0700

    updated routes in server.js, added .gets in account-router

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const welcomeRouter = require("../welcome-router");
+const accountsRouter = require("../accounts-router");
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/", welcomeRouter);
+server.use("/accounts", accountsRouter);
 
 module.exports = server;
